7ea67ed036fe8d8358b9cbc913e1f9bd33e78dce
Manjunath <manjunath.mt0@gmail.com>
2017-10-26 13:23:06 +0530

    Final stable

---
+++ src/app/User.ts
@@ -0,0 +1,17 @@
+export class User {
+
+    id: number;
+    email: string;
+    //Both the passwords are in a single object
+	password: { 
+	  pwd: string;
+	  confirmPwd: string;
+	};
+	gender: string;
+
+	constructor(values: Object = {}) {
+	  //Constructor initialization
+      Object.assign(this, values);
+  }
+
+}
